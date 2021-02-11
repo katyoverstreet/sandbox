94fb3e736e1ede06c68bfcd693299029155c3ae3
katyoverstreet <77992431+katyoverstreet@users.noreply.github.com>
2021-02-11 18:46:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+ /* Javascript goes here
+      
+      document.addEventListener("DOMContentLoaded", function() {
+        function createParagraph() {
+          let para = document.createElement('p');
+          para.textContent = 'You clicked the button!';
+          document.body.appendChild(para);
+        }
+        
+        const buttons = document.querySelectorAll('button');
+        
+        for(let i = 0; i < buttons.length ; i++) {
+          buttons[i].addEventListener('click', createParagraph);
+        }
+      });  */
+      
+      
+   function createParagraph() {
+      let para = document.createElement('p');
+      para.textContent = 'You clicked the button!';
+      document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', createParagraph);
+}
